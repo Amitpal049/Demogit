336f26705b2ca15349f6606b1b5fd8d842d917c0
amitp <amitpal049@gmail.com>
2022-03-01 02:53:38 +0530

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	void Appium1()
+	{
+		System.out.println("Appium1");
+	}
+	
+	@Test
+	void Appium2()
+	{
+		System.out.println("Appium2");
+
+	}
+
+}
---
+++ src/test/java/qaclickacademy/RestApiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	@Test
+	public void RestApiTest1()
+	{
+		System.out.println("RestApiTest1");
+	}
+	
+	@Test
+	void RestApiTest2()
+	{
+		System.out.println("RestApiTest2");
+
+	}
+
+}
